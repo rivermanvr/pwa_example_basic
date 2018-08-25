8ec11d02ff5ffa62349c2a1966f4c9d7a0dfe6d1
rivermanvr <vrgithub@riversconsulting.com>
2018-08-25 13:25:22 -0400

    setting up this example repo

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 //Progressive Enhancement (SW supported)
+
 // Check if 'service worker' is in navigator
-// if ('serviceWorker' in navigator) {}
+// if ('serviceWorker' in navigator) {} ...or... you can use the following
+
 if (navigator.serviceWorker) {
   // the method exists so register the serviceWorker
   navigator.serviceWorker.register('/sw.js')
     .then(function(registration) {
-      console.log('main.js -> SW registered');
+      console.log('main.js -> SW -- registered');
     })
     .catch(console.log);
 }
--- sw.js
+++ sw.js
@@ -1,12 +1,1 @@
-self.addEventListener('fetch', (e) => {
-  if (e.request.url.endsWith('/greet')) {
-    // if we find /greet, we will serve a message
-    // note: fetch returns a promise
-    
-    //Lets send html this time
-    let headers = new Headers({ 'Content-Type': 'text/html' });
-    let customRes = new Response('<h1>Hi Vince.  How are you today?</h1>', { headers });
-
-    e.respondWith(customRes);
-  }
-});
+console.log('SW: Registered');
