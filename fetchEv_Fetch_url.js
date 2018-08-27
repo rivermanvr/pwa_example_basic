e5282b21067e45b5bb72a8e57c55f36835597fc6
rivermanvr <vrgithub@riversconsulting.com>
2018-08-27 16:55:28 -0400

    restructuring fetch event files

--- fetchEv_Fetch_url.js
+++ fetchEv_Fetch_url.js
@@ -1,3 +1,6 @@
+console.log('SW: Fetch routine');
+
 self.addEventListener('fetch', (e) => {
   console.log('Fetch event: ' + e.request.url);
+
 });
--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 //Progressive Enhancement (SW supported)
 
 // Check if 'service worker' is in navigator
-// if ('serviceWorker' in navigator) {} ...or... you can use the following
+// if ('serviceWorker' in navigator) {} ...or... you can use
+// if (navigator.serviceWorker) {
 
-if (navigator.serviceWorker) {
-  // the method exists so register the serviceWorker
-  navigator.serviceWorker.register('/SW_Examples/sw_waitUntil_2.js')
+  if (navigator.serviceWorker) {
+  // the method exists, register the serviceWorker
+  navigator.serviceWorker.register('fetchEv_Fetch_url.js')
     .then(function(registration) {
       console.log('main.js -> SW -- registered');
     }).catch(console.log);
